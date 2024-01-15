53a9196d2b08ade6ce5dc38f538a45585ed275a8
Dima Kachurovskyy <mekaa2.718@gmail.com>
2024-01-15 11:09:47 -0500

    Editor struct; movements; n and i modes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{self, Write};
 use termion::event::{Event, Key};
 use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+use termion::raw::{IntoRawMode, RawTerminal};
 
 struct Cursor {
     x: u16,
@@ -41,29 +41,69 @@ impl Cursor {
     }
 }
 
+enum EditorModes {
+    Normal,
+    Insert,
+}
+struct Editor {
+    mode: EditorModes,
+}
+
+impl Editor {
+    fn new() -> Editor {
+        Editor {
+            mode: EditorModes::Normal,
+        }
+    }
+
+    fn run(&mut self, cursor: &mut Cursor, stdin: io::Stdin, stdout: &mut RawTerminal<io::Stdout>) {
+        writeln!(
+            stdout,
+            "{}{}{} q to exit.",
+            termion::clear::All,
+            termion::cursor::Goto(1, 1),
+            termion::cursor::SteadyBlock
+        )
+        .unwrap();
+
+        stdout.flush().unwrap();
+        let events = stdin.events();
+        for c in events {
+            let evt = c.unwrap();
+
+            match evt {
+                Event::Key(Key::Char('q')) => break,
+                Event::Key(Key::Left) => cursor.move_left(),
+                Event::Key(Key::Right) => cursor.move_right(),
+                Event::Key(Key::Up) => cursor.move_up(),
+                Event::Key(Key::Down) => cursor.move_down(),
+                Event::Key(Key::Char('i')) => match self.mode {
+                    EditorModes::Insert => {
+                        print!("{}i", termion::cursor::SteadyBar);
+                        cursor.move_right();
+                    }
+                    EditorModes::Normal => {
+                        self.mode = EditorModes::Insert;
+                        print!("{}", termion::cursor::SteadyBar)
+                    }
+                },
+                Event::Key(Key::Esc) => {
+                    self.mode = EditorModes::Normal;
+                    print!("{}", termion::cursor::SteadyBlock);
+                }
+                _ => {}
+            };
+
+            stdout.flush().unwrap();
+        }
+    }
+}
+
 fn main() {
-    let stdin = io::stdin();
     let mut stdout = io::stdout().into_raw_mode().unwrap();
+    let stdin = io::stdin();
     let mut cursor = Cursor { x: 1, y: 1 };
 
-    writeln!(
-        stdout,
-        "{}{}Ctrl+q to exit.",
-        termion::clear::All,
-        termion::cursor::Goto(cursor.x, cursor.y)
-    )
-    .unwrap();
-
-    for c in stdin.events() {
-        let evt = c.unwrap();
-        match evt {
-            Event::Key(Key::Char('q')) => break,
-            Event::Key(Key::Left) => cursor.move_left(),
-            Event::Key(Key::Right) => cursor.move_right(),
-            Event::Key(Key::Up) => cursor.move_up(),
-            Event::Key(Key::Down) => cursor.move_down(),
-            _ => {}
-        }
-        stdout.flush().unwrap();
-    }
+    let mut editor = Editor::new();
+    editor.run(&mut cursor, stdin, &mut stdout);
 }
