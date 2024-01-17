fd3ec945987a457dca128224d98d171b472563e1
Dima Kachurovskyy <mekaa2.718@gmail.com>
2024-01-17 13:42:49 -0500

    added get lines method for piece table

--- src/files/piece_table.rs
+++ src/files/piece_table.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 enum Source {
     Original,
     Add,
@@ -16,20 +18,53 @@ pub struct PieceTable {
 }
 
 impl PieceTable {
-    pub fn new(buffer: String) -> PieceTable {
+    pub fn new(buffer: &String) -> PieceTable {
         let pieces = vec![Piece {
             source: Source::Original,
             length: buffer.len(),
             start: 0,
         }];
         PieceTable {
-            original: buffer,
+            original: buffer.to_string(),
             add: String::new(),
             pieces,
         }
     }
 
-    pub fn get_lines() {}
-    pub fn edit() {}
-    pub fn delete() {}
+    pub fn get_lines(&self, from: usize, to: usize) -> VecDeque<String> {
+        let mut current_line = 1;
+        let mut res = VecDeque::with_capacity(to - from + 1 as usize);
+        let mut curr_line = String::new();
+
+        for piece in self.pieces.iter() {
+            let segment = match piece.source {
+                Source::Original => &self.original[piece.start..piece.start + piece.length],
+                Source::Add => &self.add[piece.start..piece.start + piece.length],
+            };
+
+            for char in segment.chars() {
+                if current_line >= from && to >= current_line {
+                    curr_line.push(char);
+                }
+
+                if char == '\n' {
+                    if current_line >= from && to >= current_line {
+                        res.push_back(curr_line.clone());
+                        curr_line.clear();
+                    }
+                    current_line += 1;
+                }
+
+                if current_line > to {
+                    break;
+                }
+            }
+        }
+
+        if !curr_line.is_empty() && current_line >= from && current_line <= to {
+            res.push_back(curr_line);
+        }
+
+        res
+    }
 }
